b496cb5e71dae6821169131ea07910f69e4fcba9
julian.meuleman <julian@transit32.nl>
2025-09-24 07:20:36 -0500

    test

--- terraform/nx-os/vars.tf
+++ terraform/nx-os/vars.tf
@@ -1,3 +1,3 @@
 variable "nxos_username"    { type = string }
-variable "nxos_password"    { type = string, sensitive = true }
+variable "nxos_password"    { type = string }
 variable "nxos-leaf-3_ip"   { type = string }
